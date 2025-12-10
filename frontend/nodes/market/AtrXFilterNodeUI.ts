c957ccf185234ef88be2ca43f53b69a3e8b6ce69
Quantumhurst <steve@fig.fi>
2025-12-10 09:06:09 -0800

    Fix AtrXFilter node UI expansion - disable output display
    - Set displayResults = false in AtrXFilterNodeUI constructor
    - Prevents node from expanding downward showing OHLCV bundle data
    - Matches behavior of FractalResonanceFilterNodeUI
    - Output data will only show if logging node is connected

--- frontend/nodes/market/AtrXFilterNodeUI.ts
+++ frontend/nodes/market/AtrXFilterNodeUI.ts
@@ -3,5 +3,6 @@ import BaseCustomNode from '../base/BaseCustomNode';
 export default class AtrXFilterNodeUI extends BaseCustomNode {
     constructor(title: string, data: any, serviceRegistry: any) {
         super(title, data, serviceRegistry);
+        this.displayResults = false; // Prevent UI expansion by not displaying raw output
     }
 } 
